c90ecd08554d8ec704728a1a03e584c54554db18
Shouq Almajdei <134476732+ShouqAlmajdei@users.noreply.github.com>
2023-08-25 14:06:51 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer:"A. Hyper Text Markup Language",
+        options:[
+        "A. Hyper Text Markup Language",
+        "B. Hyperlinks and Text Markup Language",
+        "C. Home Tool Markup Language",
+        "D. none of the above"
+        ]
+
+    },
+    {
+        numb: 2,
+        question:"What does CSS stand for?",
+        answer:"A. Cascading Style Sheets",
+        options:[
+        "A. Cascading Style Sheets",
+        "B. Creative Style Sheets",
+        "C. Colorful Style Sheets",
+        "D. Computer Style Sheets"
+        ]
+
+    },
+    {
+        numb: 3,
+        question:"How do you insert a comment in a CSS file?",
+        answer:"B. /*This is a comment*/",
+        options:[
+        "A. // This is a comment ",
+        "B. /*This is a comment*/",
+        "C. // This is a comment",
+        "D. All in the above"
+        ]
+
+    },
+    {
+        numb: 4,
+        question:"How do you call a function named myFunction?",
+        answer:"B. myFunction()",
+        options:[
+        "A. myFunction",
+        "B. myFunction()",
+        "C. call=myFunction",
+        "D. All in the above"
+        ]
+
+    },
+    {
+        numb: 5,
+        question:"How do you declare a JavaScript variable?",
+        answer:"A. var carName;",
+        options:[ 
+        "A. var carName;",
+        "B. v carName",
+        "C. carName;",
+        "D. variable carName;"
+        ]
+
+    }
+];
